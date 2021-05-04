79652281caff27e23f1c4259df0472c09ee80d1e
mlmargaretli6@gmail.com <>
2021-05-04 16:58:38 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
 let video;
+let poseNet;
 
 function setup() {
   console.log("I am setting up");
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded() {
+  console.log('ready');
 }
 
 function draw() {
   // background(220);
   // console.log("I am drawing");
-  image(video,0,0);
+  image(video, 0, 0);
 }
