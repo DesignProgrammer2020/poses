04f21fecb6f94a5ad54309de8c2d616988c18dc0
mlmargaretli6@gmail.com <>
2021-05-04 17:32:42 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
 let video;
 let poseNet;
+let pose = 0
 
 function setup() {
-  console.log("I am setting up");
+  // console.log("I am setting up");
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+  console.log(poses);
+  if (poses.length > 0){
+    pose = poses[0].pose;
+  }
 }
 
 function modelLoaded() {
@@ -17,4 +26,11 @@ function draw() {
   // background(220);
   // console.log("I am drawing");
   image(video, 0, 0);
+
+  if (pose){
+  noStroke();
+  fill(255, 120, 100);
+  triangle(pose.nose.x-25, pose.nose.y-25, pose.nose.x+25, pose.nose.y-25, pose.nose.x, pose.nose.y+25);
+  // circle(pose.nose.x, pose.nose.y, 64);
+  }
 }
